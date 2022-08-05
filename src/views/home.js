f647ba1bde00d730786195c23265179b14218f8e
Koteterov <koteterov@yahoo.com>
2022-08-05 11:31:07 +0300

    some styles added to home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,23 +1,19 @@
 import { html } from "../lib.js";
 
-
-
-const homeTemplate = () => html `
-        <section id="welcomePage">
-            <div class="wine-img" id="welcome-message">
-                <h1>Welcome to the Wine Catalog!</h1>
-            </div>
-
-            <div class="wine-img">
-                <img src="./images/Wine-Home.webp">
-            </div>
-        </section>
-
-
-`
-
+const homeTemplate = () => html`
+  <section id="welcomePage">
+    <div>
+      <div class="wine-img" id="welcome-message">
+        <h1>Welcome To Wine Catalog!</h1>
+      </div>
+      <div class="wine-img">
+        <img src="./images/Wine-Home.webp" />
+      </div>
+      <h2 id="loginPrompt"><a href="/login">Login</a> to add, edit and delete wines!</h2>
+    </div>
+  </section>
+`;
 
 export async function homePage(ctx) {
-    ctx.render(homeTemplate());
-
+  ctx.render(homeTemplate());
 }
