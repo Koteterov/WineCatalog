607dce35f17487a61dc79696a997bfadd2507a84
Koteterov <koteterov@yahoo.com>
2022-08-08 19:44:40 +0300

    made to directly import page and lit from CDN

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,6 @@
-import { html, render, nothing } from "../node_modules/lit-html/lit-html.js";
-import { until } from "../node_modules/lit-html/directives/until.js";
-import { classMap } from "../node_modules/lit-html/directives/class-map.js";
-import { styleMap } from "../node_modules/lit-html/directives/style-map.js"
-import { repeat } from "../node_modules/lit-html/directives/repeat.js";
-import page from "../node_modules/page/page.mjs";
+import { html, render, nothing } from 'https://unpkg.com/lit-html?module';
+import { repeat } from 'https://unpkg.com/lit-html/directives/repeat.js?module';
+import page from "//unpkg.com/page/page.mjs";
 
 
-export { html, render, nothing, until, classMap, styleMap, repeat, page };
+export { html, render, nothing, repeat, page };
